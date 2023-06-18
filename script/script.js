942257b7c1607859323ae9f96edcb1b8e36c6616
Andrey Budovsky <uyean@yandex.ru>
2023-06-18 11:58:24 +0300

    v1.06

--- script/script.js
+++ script/script.js
@@ -4,9 +4,9 @@
   const numberConver = document.getElementById('conversion-amount')
   const total = document.getElementById('total')
   const selects = document.querySelectorAll('.currency-converter__select');
-  const reversBtn = document.querySelector('.currency-converter__btn');
   const formattedDate = document.getElementById('today');
   const tableApp = document.getElementById('table-body');
+  const screenWidth = window.matchMedia("(max-width: 1024px)");
   const DEFAULT = 'Default';
   let timerId;
 
@@ -22,7 +22,7 @@
     if (serverData) {
       const currencyData = serverData.Valute;
       enumerationObjects(currencyData);
-      today(formattedDate);
+      today(formattedDate, serverData);
     }
   }
 
@@ -30,15 +30,37 @@
   function enumerationObjects(objsData) {
     selects.forEach(select => createOption(select, 'RUB', 'Российский рубль'));
 
-    for(const key in objsData) {
+    for (const key in objsData) {
       const innerObj = objsData[key];
       selects.forEach(select => createOption(select, innerObj.CharCode, innerObj.Name));
-      createTableDesktop(innerObj, tableApp);
+
+      if (window.screen.width > 1024) {
+        createTableDesktop(innerObj, tableApp);
+      } else {
+        createTableMobile(innerObj, tableApp);
+      }
     }
 
     defaultChoicesSelect(fromSelect, 'one');
     defaultChoicesSelect(toSelect, 'two');
+
+    screenWidth.addEventListener('change', (e) => {
+      if (e.matches) {
+        tableApp.innerHTML = '';
+        for (const key in objsData) {
+          const innerObj = objsData[key];
+          createTableMobile(innerObj, tableApp);
+        }
+      } else {
+        tableApp.innerHTML = '';
+        for (const key in objsData) {
+          const innerObj = objsData[key];
+          createTableDesktop(innerObj, tableApp);
+        }
+      }
+    })
   };
+
 
   // Заполнение селектора и создание элементов на основе данных API
   function createOption(container, CharCode, Name) {
@@ -59,11 +81,10 @@
   }
 
   // Получаем дату с 0 если меньше 2 знаков
-  function today(container) {
-    const todayDate = new Date();
-    const month = (todayDate.getMonth() + 1).toString().padStart(2, "0");
-    const day = todayDate.getDate().toString().padStart(2, "0");
-    container.textContent = `${day}.${month}.${todayDate.getFullYear()}`;
+  function today(container, objsData) {
+    const date = objsData.Date
+    const newDate = date.split('T')[0].split('-').reverse().join('.')
+    container.textContent = newDate;
   }
 
   // Создание таблицы на десктоп.
@@ -94,12 +115,66 @@
     container.append(tr);
   }
 
+  // Создание таблицы на мобайл
+  function createTableMobile(innerObj, container) {
+    const trOne = document.createElement('tr');
+    const trTwo = document.createElement('tr');
+    const trThree = document.createElement('tr');
+    const trFour = document.createElement('tr');
+
+    const tdNoteCode = document.createElement('td');
+    const tdCode = document.createElement('td');
+    const img = document.createElement('img');
+    const tdNoteUnit = document.createElement('td');
+    const tdUnit = document.createElement('td');
+    const tdNoteCurrency = document.createElement('td');
+    const tdCurrency = document.createElement('td');
+    const tdNoteRate = document.createElement('td');
+    const tdRate = document.createElement('td');
+
+    trOne.classList.add('table__tr-mobile');
+    trTwo.classList.add('table__tr-mobile');
+    trThree.classList.add('table__tr-mobile');
+    trFour.classList.add('table__tr-mobile');
+
+    tdNoteCode.classList.add('table__td-mobile');
+    tdCode.classList.add('table__td-mobile', 'table__td-mobile--code');
+    img.classList.add('table__img');
+    tdNoteUnit.classList.add('table__td-mobile');
+    tdUnit.classList.add('table__td-mobile', 'table__td-mobile--unit');
+    tdNoteCurrency.classList.add('table__td-mobile');
+    tdCurrency.classList.add('table__td-mobile', 'table__td-mobile--currency');
+    tdNoteRate.classList.add('table__td-mobile');
+    tdRate.classList.add('table__td-mobile', 'table__td-mobile--rate');
+
+    tdNoteCode.textContent = 'Код';
+    tdNoteUnit.textContent = 'Единица';
+    tdNoteCurrency.textContent = 'Валюта';
+    tdNoteRate.textContent = 'Курс базовой валюты';
+
+    tdCode.textContent = innerObj.CharCode;
+    tdUnit.textContent = innerObj.Nominal;
+    tdCurrency.textContent = innerObj.Name;
+    tdRate.textContent = innerObj.Value;
+
+    img.src = `img/flags/${innerObj.CharCode}.svg`;
+    img.alt = `Флаг страны ${innerObj.CharCode}`;
+
+    tdCode.prepend(img);
+    trOne.append(tdNoteCode, tdCode);
+    trTwo.append(tdNoteUnit, tdUnit);
+    trThree.append(tdNoteCurrency, tdCurrency);
+    trFour.append(tdNoteRate, tdRate);
+    container.append(trOne, trTwo, trThree, trFour);
+  }
+
   // Кастомный селект
   function defaultChoicesSelect(select, num) {
     const choices = new Choices(select, {
       searchEnabled: true,
       position: 'down',
       noResultsText: 'Валюта не найдена',
+      searchResultLimit: 44,
       classNames: {
         listDropdown: `choices__list--dropdown-${num}`,
       }
@@ -133,15 +208,6 @@
       }
     })
   })
-
-  reversBtn.addEventListener('click', () => {
-    const selectOne = choicesOne.getValue();
-    const selectTwo = choicesTwo.getValue();
-
-    choicesOne.setValue(selectTwo)
-    choicesTwo.setValue(selectOne)
-  })
-
 
   window.api = api;
 })();
