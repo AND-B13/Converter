d4f4585c14c4b00337e38e6e33ba7366825e8367
Andrey Budovsky <uyean@yandex.ru>
2023-06-21 14:18:15 +0300

    v1.14
    JS

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@
   const mainPage = document.getElementById('page');
   const fromSelect = document.getElementById('select-from');
   const toSelect = document.getElementById('select-to');
-  const numberConver = document.getElementById('conversion-amount');
+  const conversionAmount = document.getElementById('conversion-amount');
   const total = document.getElementById('total');
   const selects = document.querySelectorAll('.currency-converter__select');
   const formattedDate = document.getElementById('today');
@@ -13,7 +13,7 @@
   const strips = document.querySelectorAll('.spinner__strip');
   const screenWidth = window.matchMedia("(max-width: 1024px)");
   const screenWidthMobileBtn = window.matchMedia("(max-width: 576px)");
-  const DEFAULT = 'Default';
+  const defaultValue = 'Default';
   let timerId;
 
   // Получение данных API
@@ -22,13 +22,15 @@
     return await response.json();
   }
 
-  // Вызвана из HTML, получает данные с API, если они есть отправляет на перебор + ставит актуальную дату.
+  // Вызвана из HTML, получает данные с API, если они есть отправляет по функциям, ставит актуальную дату и снимаем загрузочный экран.
   async function api() {
     const serverData = await getData();
     if (serverData) {
       const currencyData = serverData.Valute;
-      enumerationObjects(currencyData);
-      today(formattedDate, serverData);
+      goToCreateSelect(currencyData, selects);
+      goToCreateChoices(fromSelect, toSelect);
+      goToCreateTable(currencyData, tableApp);
+      formatDate(serverData.Date, formattedDate);
       setTimeout(() => {
         hideLoader();
       }, 2000);
@@ -45,63 +47,20 @@
     })
   }
 
-  // Перебор данных с API и распределение на отрисовку
-  function enumerationObjects(objsData) {
-    selects.forEach(select => createOption(select, 'RUB', 'Российский рубль'));
+  // Получаем дату с 0 если меньше 2 знаков
+  function formatDate(date, container) {
+    const newDate = date.split('T')[0].split('-').reverse().join('.')
+    container.textContent = newDate;
+  }
+
+  function goToCreateSelect(objsData, container) {
+    container.forEach(select => createOption(select, 'RUB', 'Российский рубль'));
 
     for (const key in objsData) {
       const innerObj = objsData[key];
-      selects.forEach(select => {
-        createOption(select, innerObj.CharCode, innerObj.Name);
-      });
-
-      if (window.screen.width > 1024) {
-        createTableDesktop(innerObj, tableApp);
-      } else {
-        createTableMobile(innerObj, tableApp);
-      }
+      container.forEach(select => createOption(select, innerObj.CharCode, innerObj.Name));
     }
-
-    const choicesOne = defaultChoicesSelect(fromSelect, 'one');
-    const choicesTwo = defaultChoicesSelect(toSelect, 'two');
-
-    if (window.screen.width < 577) {
-      closeBtnMobile();
-    }
-
-    screenWidth.addEventListener('change', (e) => {
-      if (e.matches) {
-        tableApp.innerHTML = '';
-        for (const key in objsData) {
-          const innerObj = objsData[key];
-          createTableMobile(innerObj, tableApp);
-        }
-      } else {
-        tableApp.innerHTML = '';
-        for (const key in objsData) {
-          const innerObj = objsData[key];
-          createTableDesktop(innerObj, tableApp);
-        }
-      }
-    })
-
-    reverseBtn.addEventListener('click', () => {
-      numberConver.value = '';
-      total.textContent = '';
-
-      const selectOne = choicesOne.getValue();
-      const selectTwo = choicesTwo.getValue();
-
-      choicesOne.setValue([{
-        value: selectTwo.value,
-        label: selectTwo.label,
-      }])
-      choicesTwo.setValue([{
-        value: selectOne.value,
-        label: selectOne.label,
-      }])
-    })
-  };
+  }
 
   // Заполнение селектора и создание элементов на основе данных API
   function createOption(container, CharCode, Name) {
@@ -121,96 +80,144 @@
     container.append(elementOption);
   }
 
-  // Получаем дату с 0 если меньше 2 знаков
-  function today(container, objsData) {
-    const date = objsData.Date
-    const newDate = date.split('T')[0].split('-').reverse().join('.')
-    container.textContent = newDate;
+  function goToCreateChoices(from, to) {
+    const choicesOne = createChoices(from, 'one');
+    const choicesTwo = createChoices(to, 'two');
+
+    if (screen.width < 577) {
+      closeBtnMobile();
+    }
+
+    reverseBtn.addEventListener('click', () => {
+      conversionAmount.value = '';
+      total.textContent = '';
+
+      const selectOne = choicesOne.getValue();
+      const selectTwo = choicesTwo.getValue();
+
+      choicesOne.setValue([{
+        value: selectTwo.value,
+        label: selectTwo.label,
+      }])
+      choicesTwo.setValue([{
+        value: selectOne.value,
+        label: selectOne.label,
+      }])
+    })
   }
 
-  // Создание таблицы на десктоп.
-  function createTableDesktop(innerObj, container) {
-    const tr = document.createElement('tr');
-    const tdCode = document.createElement('td');
-    const img = document.createElement('img');
-    const tdUnit = document.createElement('td');
-    const tdCurrency = document.createElement('td');
-    const tdRate = document.createElement('td');
+  function goToCreateTable(objsData, container) {
+    if (screen.width > 1024) {
+      for (const key in objsData) {
+        const innerObj = objsData[key];
+        createTable(innerObj, container, 'desktop');
+      }
+    } else {
+      for (const key in objsData) {
+        const innerObj = objsData[key];
+        createTable(innerObj, container, 'mobile');
+      }
+    }
 
-    tr.classList.add('table__tr');
-    tdCode.classList.add('table__td');
-    img.classList.add('table__img');
-    tdUnit.classList.add('table__td');
-    tdCurrency.classList.add('table__td');
-    tdRate.classList.add('table__td');
-
-    img.src = `img/flags/${innerObj.CharCode}.svg`;
-    img.alt = `Флаг страны ${innerObj.CharCode}`;
-
-    tdCode.textContent = innerObj.CharCode;
-    tdUnit.textContent = innerObj.Nominal;
-    tdCurrency.textContent = innerObj.Name;
-    tdRate.textContent = innerObj.Value;
-    tdCode.prepend(img)
-    tr.append(tdCode, tdUnit, tdCurrency, tdRate)
-    container.append(tr);
+    screenWidth.addEventListener('change', (e) => {
+      if (e.matches) {
+        container.innerHTML = '';
+        for (const key in objsData) {
+          const innerObj = objsData[key];
+          createTable(innerObj, container, 'mobile');
+        }
+      } else {
+        container.innerHTML = '';
+        for (const key in objsData) {
+          const innerObj = objsData[key];
+          createTable(innerObj, container, 'desktop');
+        }
+      }
+    })
   }
 
-  // Создание таблицы на мобайл
-  function createTableMobile(innerObj, container) {
-    const trOne = document.createElement('tr');
-    const trTwo = document.createElement('tr');
-    const trThree = document.createElement('tr');
-    const trFour = document.createElement('tr');
+  // Создание таблицы
+  function createTable(innerObj, container, format) {
+    if (format === 'desktop') {
+      const tr = document.createElement('tr');
+      const tdCode = document.createElement('td');
+      const img = document.createElement('img');
+      const tdUnit = document.createElement('td');
+      const tdCurrency = document.createElement('td');
+      const tdRate = document.createElement('td');
 
-    const tdNoteCode = document.createElement('td');
-    const tdCode = document.createElement('td');
-    const img = document.createElement('img');
-    const tdNoteUnit = document.createElement('td');
-    const tdUnit = document.createElement('td');
-    const tdNoteCurrency = document.createElement('td');
-    const tdCurrency = document.createElement('td');
-    const tdNoteRate = document.createElement('td');
-    const tdRate = document.createElement('td');
+      tr.classList.add('table__tr');
+      tdCode.classList.add('table__td');
+      img.classList.add('table__img');
+      tdUnit.classList.add('table__td');
+      tdCurrency.classList.add('table__td');
+      tdRate.classList.add('table__td');
 
-    trOne.classList.add('table__tr-mobile');
-    trTwo.classList.add('table__tr-mobile');
-    trThree.classList.add('table__tr-mobile');
-    trFour.classList.add('table__tr-mobile');
+      img.src = `img/flags/${innerObj.CharCode}.svg`;
+      img.alt = `Флаг страны ${innerObj.CharCode}`;
 
-    tdNoteCode.classList.add('table__td-mobile');
-    tdCode.classList.add('table__td-mobile', 'table__td-mobile--code');
-    img.classList.add('table__img');
-    tdNoteUnit.classList.add('table__td-mobile');
-    tdUnit.classList.add('table__td-mobile', 'table__td-mobile--unit');
-    tdNoteCurrency.classList.add('table__td-mobile');
-    tdCurrency.classList.add('table__td-mobile', 'table__td-mobile--currency');
-    tdNoteRate.classList.add('table__td-mobile');
-    tdRate.classList.add('table__td-mobile', 'table__td-mobile--rate');
+      tdCode.textContent = innerObj.CharCode;
+      tdUnit.textContent = innerObj.Nominal;
+      tdCurrency.textContent = innerObj.Name;
+      tdRate.textContent = innerObj.Value;
+      tdCode.prepend(img)
+      tr.append(tdCode, tdUnit, tdCurrency, tdRate)
+      container.append(tr);
+    } else {
+      const trOne = document.createElement('tr');
+      const trTwo = document.createElement('tr');
+      const trThree = document.createElement('tr');
+      const trFour = document.createElement('tr');
 
-    tdNoteCode.textContent = 'Код';
-    tdNoteUnit.textContent = 'Единица';
-    tdNoteCurrency.textContent = 'Валюта';
-    tdNoteRate.textContent = 'Курс базовой валюты';
+      const tdNoteCode = document.createElement('td');
+      const tdCode = document.createElement('td');
+      const img = document.createElement('img');
+      const tdNoteUnit = document.createElement('td');
+      const tdUnit = document.createElement('td');
+      const tdNoteCurrency = document.createElement('td');
+      const tdCurrency = document.createElement('td');
+      const tdNoteRate = document.createElement('td');
+      const tdRate = document.createElement('td');
 
-    tdCode.textContent = innerObj.CharCode;
-    tdUnit.textContent = innerObj.Nominal;
-    tdCurrency.textContent = innerObj.Name;
-    tdRate.textContent = innerObj.Value;
+      trOne.classList.add('table__tr-mobile');
+      trTwo.classList.add('table__tr-mobile');
+      trThree.classList.add('table__tr-mobile');
+      trFour.classList.add('table__tr-mobile');
 
-    img.src = `img/flags/${innerObj.CharCode}.svg`;
-    img.alt = `Флаг страны ${innerObj.CharCode}`;
+      tdNoteCode.classList.add('table__td-mobile');
+      tdCode.classList.add('table__td-mobile', 'table__td-mobile--code');
+      img.classList.add('table__img');
+      tdNoteUnit.classList.add('table__td-mobile');
+      tdUnit.classList.add('table__td-mobile', 'table__td-mobile--unit');
+      tdNoteCurrency.classList.add('table__td-mobile');
+      tdCurrency.classList.add('table__td-mobile', 'table__td-mobile--currency');
+      tdNoteRate.classList.add('table__td-mobile');
+      tdRate.classList.add('table__td-mobile', 'table__td-mobile--rate');
 
-    tdCode.prepend(img);
-    trOne.append(tdNoteCode, tdCode);
-    trTwo.append(tdNoteUnit, tdUnit);
-    trThree.append(tdNoteCurrency, tdCurrency);
-    trFour.append(tdNoteRate, tdRate);
-    container.append(trOne, trTwo, trThree, trFour);
+      tdNoteCode.textContent = 'Код';
+      tdNoteUnit.textContent = 'Единица';
+      tdNoteCurrency.textContent = 'Валюта';
+      tdNoteRate.textContent = 'Курс базовой валюты';
+
+      tdCode.textContent = innerObj.CharCode;
+      tdUnit.textContent = innerObj.Nominal;
+      tdCurrency.textContent = innerObj.Name;
+      tdRate.textContent = innerObj.Value;
+
+      img.src = `img/flags/${innerObj.CharCode}.svg`;
+      img.alt = `Флаг страны ${innerObj.CharCode}`;
+
+      tdCode.prepend(img);
+      trOne.append(tdNoteCode, tdCode);
+      trTwo.append(tdNoteUnit, tdUnit);
+      trThree.append(tdNoteCurrency, tdCurrency);
+      trFour.append(tdNoteRate, tdRate);
+      container.append(trOne, trTwo, trThree, trFour);
+    }
   }
 
   // Кастомный селект
-  function defaultChoicesSelect(select, num) {
+  function createChoices(select, num) {
     const choices = new Choices(select, {
       searchEnabled: true,
       position: 'down',
@@ -250,7 +257,6 @@
       select.addEventListener('click', () => {
         closeBtn.classList.remove('currency-converter__btn-close--hidden');
         closeBtn.addEventListener('click', () => {
-          select.classList.remove('is-open');
           closeBtn.classList.add('currency-converter__btn-close--hidden');
         })
       })
@@ -265,26 +271,25 @@
   })
 
   // Событие с проверкой для отправки данных на конвертацию + задержка в 0.5s
-  numberConver.addEventListener('input', () => {
-    if (fromSelect.value !== DEFAULT && toSelect.value !== DEFAULT) {
+  conversionAmount.addEventListener('input', () => {
+    if (fromSelect.value !== defaultValue && toSelect.value !== defaultValue) {
       clearTimeout(timerId);
       timerId = setTimeout(() => {
-        conversion(numberConver.value, fromSelect.value, toSelect.value, total);
-        console.log(fromSelect.value);
+        conversion(conversionAmount.value, fromSelect.value, toSelect.value, total);
       }, 500);
     }
   });
 
   // Пропкуск буквы e и E для числового ввода
-  numberConver.addEventListener('keypress', (e) => {
+  conversionAmount.addEventListener('keypress', (e) => {
     if (e.key === 'e' || e.key === 'E') e.preventDefault()
   })
 
   // Доп проверка на селекторы если инпут введен раньше выбора
   selects.forEach(select => {
     select.addEventListener('change', () => {
-      if (fromSelect.value !== DEFAULT && toSelect.value !== DEFAULT && numberConver.value) {
-        conversion(numberConver.value, fromSelect.value, toSelect.value, total)
+      if (fromSelect.value !== defaultValue && toSelect.value !== defaultValue && conversionAmount.value) {
+        conversion(conversionAmount.value, fromSelect.value, toSelect.value, total)
       }
     })
   })
